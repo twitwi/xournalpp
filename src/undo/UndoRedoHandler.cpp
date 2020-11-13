f85ab36c798ada72867e80af94dcfae0e1e8a0bb
Rémi Emonet <remi.emonet@reverse--com.heeere>
2020-11-13 22:04:55 +0100

    Version replicating the clipboard function (but pb with refresh)

--- src/undo/UndoRedoHandler.cpp
+++ src/undo/UndoRedoHandler.cpp
@@ -10,12 +10,17 @@
 #include "config.h"
 #include "i18n.h"
 
+#include "AddUndoAction.h"
 #include "ColorUndoAction.h"
 #include "InsertUndoAction.h"
 #include "serializing/BinObjectEncoding.h"
 #include "serializing/HexObjectEncoding.h"
 #include "serializing/ObjectInputStream.h"
 #include "serializing/ObjectOutputStream.h"
+#include "model/Text.h"
+#include "model/TexImage.h"
+#include "model/Image.h"
+#include "model/Stroke.h"
 #include "control/tools/EditSelection.h"
 #include "gui/XournalView.h"
 
@@ -68,13 +73,16 @@ void UndoRedoHandler::printContents() {
 }
 
 void handle(const char* what, const UndoAction& action, Control* control) {
+
+    // one small problem is that pasting moves the elements at the center
+
     auto* ap = &action;
     g_message(what);
     g_message(action.getClassName().c_str());
     if (auto a = dynamic_cast<const ColorUndoAction*>(ap)) {
         g_message("OK COLOR");
 
-        FILE* pFile = fopen(".xournal-test-binary.bin3", "rb");
+        FILE* pFile = fopen(".xournal-test-binary.bin5", "rb");
         //if (pFile==NULL) {fputs ("File error",stderr); exit (1);}
         // I'd rather crash
 
@@ -93,8 +101,64 @@ void handle(const char* what, const UndoAction& action, Control* control) {
 
         ObjectInputStream in;
         if (in.read(buffer, lSize)) {
-            control->clipboardPasteXournal(in); // why not refreshing???
-            control->clearSelection(); // incurs a refresh, ok
+
+            //control->getDocument()->lock();
+            int pageNr = control->getCurrentPageNo();
+            PageRef page = control->getCurrentPage();
+            MainWindow* win = control->getWindow();
+            XojPageView* view = win->getXournal()->getViewFor(pageNr);
+            auto undoRedo = control->getUndoRedoHandler();
+            auto selection = new EditSelection(undoRedo, page, view);
+
+            win->getXournal()->setSelection(selection);
+
+            std::unique_ptr<Element> element;
+            string version = in.readString();
+            if (version != PROJECT_STRING) {
+                g_warning("Sync from Xournal Version %s to Xournal Version %s", version.c_str(), PROJECT_STRING);
+            }
+
+            selection->readSerialized(in);
+            //control->getDocument()->unlock();
+
+            int count = in.readInt();
+            Layer* layer = page->getSelectedLayer();
+            auto syncAddUndoAction = std::make_unique<AddUndoAction>(page, false);
+
+            for (int i = 0; i < count; i++) {
+                string name = in.getNextObjectName();
+                element.reset();
+
+                if (name == "Stroke") {
+                    element = std::make_unique<Stroke>();
+                } else if (name == "Image") {
+                    element = std::make_unique<Image>();
+                } else if (name == "TexImage") {
+                    element = std::make_unique<TexImage>();
+                } else if (name == "Text") {
+                    element = std::make_unique<Text>();
+                } else {
+                    throw InputStreamException(FS(FORMAT_STR("Get unknown object {1}") % name), __FILE__, __LINE__);
+                }
+
+                element->readSerialized(in);
+
+                syncAddUndoAction->addElement(layer, element.get(), layer->indexOf(element.get()));
+                // Todo: unique_ptr
+                selection->addElement(element.release(), Layer::InvalidElementIndex);
+            }
+            g_message("ADD UNDO ACTION");
+            undoRedo->addUndoAction(std::move(syncAddUndoAction));
+
+            g_message("CLEAR SEL");
+            control->clearSelection();
+            //selection->moveSelection(0, 0);
+            //selection->mouseUp();
+            //win->getXournal()->repaintSelection();
+            control->clearSelection(); // causes a refresh, ok
+
+            //control->clipboardPasteXournal(in); // why not refreshing???
+            //control->clearSelection(); // incurs a refresh, ok
             //control->getWindow()->getXournal()->getSelection();
             // this->page->fireElementChanged(this->element);
         }
@@ -109,9 +173,9 @@ void handle(const char* what, const UndoAction& action, Control* control) {
         out.writeString(PROJECT_STRING);
 
         int pageNr = control->getCurrentPageNo();
-        auto* win = control->getWindow();
-        auto* view = win->getXournal()->getViewFor(pageNr);
-        auto page = control->getCurrentPage();
+        MainWindow* win = control->getWindow();
+        XojPageView* view = win->getXournal()->getViewFor(pageNr);
+        PageRef page = control->getCurrentPage();
         auto sel = new EditSelection(control->getUndoRedoHandler(), page, view);
         sel->addElement(e);
         sel->serialize(out);
@@ -177,7 +241,7 @@ void UndoRedoHandler::undo() {
     g_assert_true(this->undoList.back());
 
     auto& undoAction = *this->undoList.back();
-    handle("UNDOING", undoAction, control);
+    //handle("UNDOING", undoAction, control);
 
     this->redoList.emplace_back(std::move(this->undoList.back()));
     this->undoList.pop_back();
@@ -207,7 +271,7 @@ void UndoRedoHandler::redo() {
     g_assert_true(this->redoList.back());
 
     UndoAction& redoAction = *this->redoList.back();
-    handle("REDOING", redoAction, control);
+    //handle("REDOING", redoAction, control);
 
     this->undoList.emplace_back(std::move(this->redoList.back()));
     this->redoList.pop_back();
